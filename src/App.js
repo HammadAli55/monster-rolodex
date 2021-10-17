3215c8f25ab540663c112da1e0133a0b333bc558
hammadali55 <hammadalii@outlook.com>
2021-10-18 00:50:35 +0500

    create card-list component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-
+import { CardList } from './components/card-list/card-list.component';
+// class component
 class App extends Component{
   constructor(){
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
   }
   
   render() {
     return (
       // JSX code
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1> {monster.name} </h1>)
-        }
+        <CardList name="Shahh ">
+          {
+            this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
+          }
+        </CardList>
       </div>
     );
   }
 }
 
+// functional component
 // function App() {
 //   return (
 //     <div className="App">
