52de3c8b5a5033ca8d60c03775bd607427f80144
hammadali55 <hammadalii@outlook.com>
2022-03-28 23:17:41 +0500

    create card-list component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component';
-// class component
-class App extends Component{
+import { CardList } from './components/card-list/card-list.component.jsx'
+
+// function App() {
+//   return(
+//     <div>
+//       <p>Lol</p>
+//     </div>
+//   )
+// }
+
+class App extends Component {
   constructor(){
     super();
-
+    // this.state = {
+    //   string: "Hammad"
+    // }
     this.state = {
-      monsters: [],
-      searchFiled: ''
+      monsters : []
     }
   }
 
   componentDidMount() {
+    //fetch return promise
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({monsters: users}))
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
   }
-  
+
   render() {
-    return (
-      // JSX code
-      <div className='App'>
-        <input type='search' placeholder='search monster' onChange={ e => 
-            this.setState({searchFiled: e.target.value}) 
-        }/>
-        <CardList monsters={this.state.monsters} />
+    return(
+      <div>
+        <CardList monsters = {this.state.monsters}/>
       </div>
-    );
+      )
   }
 
 }
-
-// functional component
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
