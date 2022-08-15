b504600db00d0fa125579a6f353848d69cc202e5
hammadali55 <hammadalii@outlook.com>
2022-08-15 16:29:13 +0500

    add comments in App component

--- src/App.js
+++ src/App.js
@@ -11,20 +11,23 @@ class App extends Component {
     }
   }
 
+  //componentDidMount() is invoked immediately after a component is mounted (inserted into the tree). 
+  //Initialization that requires DOM nodes should go here.
   componentDidMount() {
     //fetch return promise
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))
+    // setState() schedules an update to a component's state object. When state changes, the component responds by re-rendering.
   }
 
   render() {
     const {monsters, searchField} = this.state;
     const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return(
-      <div className='App'>
-        <input type="search" placeholder="Search Monsters" 
-          onChange={e => {
+      <div className = 'App'>
+        <input type = "search" placeholder = "Search Monsters" 
+          onChange = {e => {
             this.setState({searchField: e.target.value}, () => this.state)
           }}/>
         <CardList monsters = {filterMonsters}/>
